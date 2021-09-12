b43ff66f8d25faae649a68a3a3da34136111ef91
Samuel Martins <fsamuelsmartins@gmail.com>
2021-09-12 12:50:50 -0300

    Banco h2 foi adicionado

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO currency_conversions (from_currency,to_currency, exchange_value) VALUES
+    ('usd', 'brl', 5.25),
+    ('brl', 'usd', 0.19),
+    ('eu', 'usd', 1.18),
+    ('usd', 'eu', 0.85),
+    ('eu', 'brl', 6.20),
+    ('brl', 'eu', 0.16);
