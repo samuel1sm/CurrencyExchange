4557d9e1386a10c9604258adf7c54cab887ced3a
Samuel Martins <fsamuelsmartins@gmail.com>
2021-10-03 20:17:32 -0300

    tests

---
+++ src/test/java/com/example/trabalhotcc2/CurrencyTestsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.trabalhotcc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class CurrencyTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Boolean result = true;
+
+		Assert.isTrue(result);
+	}
+
+}
